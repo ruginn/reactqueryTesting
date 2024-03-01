e000a220ba1bc09f144781e0eee11fb1f9937784
Rcatarata <Rcatarata@gmail.com>
2024-02-29 20:20:20 -0500

    Update App.tsx

--- ReactQueryTesting/src/App.tsx
+++ ReactQueryTesting/src/App.tsx
@@ -29,6 +29,7 @@ function App() {
       fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify(newPost),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
       }).then((res) => res.json()),
   });
 
